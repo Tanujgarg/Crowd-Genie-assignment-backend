7b49f0b200348e5843f2f0c3d3749165e61beba2
Tanuj Garg <tanujgarg@y7mail.com>
2018-10-04 14:53:54 +0530

    some domains added to load application model

--- models/application.js
+++ models/application.js
@@ -2,7 +2,22 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 
+const durationSchema = new Schema({
+    value: Number,
+    units: {
+        type: String,
+        default: "MONTHS"
+    }
+})
+
 const applicationSchema = new Schema({
-    _userId = Schema.Types.ObjectId,
-    
+    _userId: Schema.Types.ObjectId,
+    amount: Number,
+    currency: {
+        type: String,
+        default: "INR"
+    },
+    gracePeriod: durationSchema,
+    loanType: String,
+    interestRate: Number,
 })
