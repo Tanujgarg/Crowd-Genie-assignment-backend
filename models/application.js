4de1a686a0fcbbc67ef92a76c4557c54e8f8c8c1
Tanuj Garg <tanujgarg@y7mail.com>
2018-10-04 14:26:45 +0530

    initial step for loan application schema

---
+++ models/application.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+const applicationSchema = new Schema({
+    _userId = Schema.Types.ObjectId,
+    
+})
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
 const nameSchema = new Schema({
     firstName: String,
@@ -49,4 +49,4 @@ const userSchema = new Schema({
 
 const User = mongoose.model("User", userSchema)
 
-exports.User = User;
+exports.User = User
